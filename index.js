2699c20b5775de9ba973a6831c35d2e602255cbb
Leilani <pquaby@gmail.com>
2024-02-21 13:14:17 -0500

    added test file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { fetchBreedDescription } = require('./breedFetcher');
 
 //Get input from command line
 const breed = process.argv.slice(2);
+
 //Capitlize first letter and change rest to lower case
 const breedName = breed[0].charAt(0).toUpperCase() + breed[0].slice(1).toLowerCase();
 
