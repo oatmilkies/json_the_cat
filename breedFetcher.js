90c772befeae2cc1c4fbc49106ee018a5852e6c5
Leilani <pquaby@gmail.com>
2024-02-21 12:26:38 -0500

    added error message

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,15 +6,17 @@ const breed = process.argv.slice(2);
 //Capitlize first letter and change rest to lower case
 const formattedBreed = breed[0].charAt(0).toUpperCase() + breed[0].slice(1).toLowerCase();
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${formattedBreed}`, (error, response, body) => {
-  const data = JSON.parse(body); //deserialization - convert string to object
-
-  if (data[0] === undefined) {
-    //Print error if breed not found
-    console.log(`Breed ${formattedBreed} not found`);
-  } else
-    //Print the breed description
-    console.log(data[0].description);
-
-
+request(`https://api.thecatap.com/v1/breeds/search?q=${formattedBreed}`, (error, response, body) => {
+  //If there's an error, print error message
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    const data = JSON.parse(body); //deserialization - convert string to object
+    if (data[0] === undefined) {
+      //Print error if breed not found
+      console.log(`Breed ${formattedBreed} not found`);
+    } else
+      //Print the breed description
+      console.log(data[0].description);
+  }
 });
